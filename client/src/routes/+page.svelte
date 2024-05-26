565ebb1af06a21e4ad53d89c74cc3b68d2781b17
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-05-27 01:48:05 +0530

    further nodes

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -13,24 +13,55 @@
 
     let iActions: Array<string>;
     $: iActions
+
+    let node_counter = 0
     
     const add_action = () => {
-        const actions_div = document.getElementsByClassName("all_initial_actions")[0]
+        if (node_counter == 0) node_counter++
+        const actions_div = document.getElementsByClassName("action_div")[node_counter-1]
 
         const action_elem = document.createElement("input")
         action_elem.setAttribute("type", "text")
-        action_elem.setAttribute("class", "initial_action")
-        action_elem.className = "initial_actions"
-
+        action_elem.className = "actions"
+        
         actions_div?.appendChild(action_elem)
     }
-
+    
     const add_move = () => {
-        const first_action_btn = document.getElementById("initial_action_btn")
-        first_action_btn?.setAttribute("disabled", "true")
+        node_counter++
+        const old_action_btn = Array.from(document.getElementsByClassName("action_btn")).at(-1)
+        old_action_btn?.setAttribute("disabled", "true")
+        
+        // const iLen = iActions.length
+        // iActions.push(...Array.from(document.getElementsByClassName("actions") , x => (x as HTMLInputElement).value).slice(iLen))
+        
+        const further_div = document.getElementsByClassName("further")[0]
 
-        iActions = Array.from(document.getElementsByClassName("initial_actions") , x => (x as HTMLInputElement).value);
-        console.log(iActions)
+        const ind_div = document.createElement("div")
+        ind_div.className = "ind_div"
+        ind_div.style.display = "flex"
+        ind_div.style.flexDirection = "row"
+
+        further_div.appendChild(ind_div)
+
+        const player_elem = document.createElement("input")
+        player_elem.setAttribute("type", "number")
+        player_elem.setAttribute("placeholder", "Player")
+        player_elem.className = "player_input"
+
+        ind_div.appendChild(player_elem)
+
+        const action_btn = document.createElement("button")
+        action_btn.textContent = "Add action"
+        action_btn.className = "action_btn"
+        action_btn.onclick = add_action
+
+        ind_div.appendChild(action_btn)
+
+        const action_div = document.createElement("div")
+        action_div.className = "action_div"
+
+        ind_div.appendChild(action_div)
     }
 
 </script>
@@ -43,12 +74,16 @@
 <div class="tree">
     <div class="initial">
         Initial node
-        <input type="number" name="initial_player" id="initial_player" bind:value={initial_player} required placeholder="Player" />
-        <button type="button" id="initial_action_btn" on:click={add_action}>Add action</button>
-        <div class="all_initial_actions"></div>
+        <input type="number" name="player_input" id="initial_player" bind:value={initial_player} required placeholder="Player" />
+        <button type="button" class="action_btn" on:click={add_action}>Add action</button>
+        <div class="action_div"></div>
     </div>
     <div class="further">
-        <button type="button" id="node_btn" on:click={add_move}>Add move</button>
+        {#if node_counter < 1}
+            <button type="button" id="node_btn" on:click={add_move} disabled>Add move</button>
+        {:else}
+            <button type="button" id="node_btn" on:click={add_move}>Add move</button>
+        {/if}
     </div>
 </div>
 
@@ -59,7 +94,7 @@
         gap: 10px
     }
     
-    .all_initial_actions {
+    .action_div {
         display: flex;
         flex-direction: row;
         gap: 10px
