aa3529f47f337990f05abc9b0efaa50c3b9fd49f
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-02 21:18:07 +0530

    multiple spne: final payoffs

--- server/api/solver.py
+++ server/api/solver.py
@@ -23,7 +23,7 @@ class Node:
             self, 
             node_number: int, 
             player: int = None,
-            children: list["Node"] = None,
+            children: list[list["Node"]] = None, # interpret as all possible children
             actions: tuple[str] = None,
             action: list[str] = None,
             payoffs: tuple[int] = None
@@ -36,27 +36,53 @@ class Node:
         self.action = action
         self.payoffs = payoffs
 
-def subnash(node: "Node"):
-    sub_node: "Node" = ()
-    max_payoff = [float("-inf") for i in node.children[0].payoffs]
-    action = ""
+def all_perms(max_ls):
+    if len(max_ls) == 1:
+        return list(map(lambda x: [x], list(range(max_ls[0] + 1))))
+    prev = all_perms(max_ls[1:])
+    cr = []
+    for i in range(max_ls[0] + 1):
+        for j in range(len(prev)):
+            cr.append([i, *prev[j]])
+    return cr
 
-    for i in range(len(node.children)):
-        np = node.children[i].payoffs[node.player]
-        if np > max_payoff[node.player]:
-            sub_node = node.children[i]
+def subnash(node: "Node"):
+    ch = node.children
+    maxp = [len(i)-1 for i in ch]
+    perms = all_perms(maxp)
+
+    # sub_nodes: set["Node"] = set()
+    sub_nodes: list["Node"] = []
+    for p in range(len(perms)):
+        # internal_sub = set()
+        internal_sub = []
+        sub_node: "Node" = ()
+        max_payoff = [float("-inf") for i in node.children[0][0].payoffs]
+        action = ""
+        for i in range(len(perms[p])):
+            np = node.children[i][perms[p][i]].payoffs[node.player]
+            if np < max_payoff[node.player]:
+                continue
+            elif np > max_payoff[node.player]:
+                internal_sub = []
+            sub_node = node.children[i][perms[p][i]]
             action = node.actions[i]
             max_payoff = sub_node.payoffs
     
-    return action, max_payoff, node.player, node.node_number, sub_node
+            internal_sub.append(sub_node)
+        
+        for i in range(len(internal_sub)):
+            sub_nodes.append(internal_sub[i])
+
+    return action, max_payoff, node.player, node.node_number, sub_nodes
 
 def spne(arr: list, node: "Node"):
     children = node.children
     main_arr = []
     for i in range(len(children)):
-        if children[i].payoffs == None:
-            ret, ret_arr = spne(arr, children[i]) # recursively make all pre-terminal nodes
-            children[i] = ret
+        if children[i][0].payoffs == None:
+            ret, ret_arr = spne(arr, children[i][0]) # recursively make all pre-terminal nodes
+            children[i] = ret # automatically propagates to node.children
             main_arr.extend(ret_arr)
     action, payoff, player, num, n = subnash(node)
     main_arr.append({
@@ -73,69 +99,73 @@ def main():
     # n0 = Node(node_number=0, player=0, children=[], actions=("S", "C"))
 
     # n1 = Node(node_number=1, player=1, children=[], actions=("S", "C"))
-    # n0.children.append(n1)
+    # n0.children.append([n1])
 
     # n2 = Node(node_number=2, player=1, children=[], actions=("S", "C"))
-    # n0.children.append(n2)
+    # n0.children.append([n2])
 
     # n3 = Node(node_number=3, payoffs=(2,1))
-    # n1.children.append(n3)
+    # n1.children.append([n3])
 
     # n4 = Node(node_number=4, payoffs=(0,0))
-    # n1.children.append(n4)
+    # n1.children.append([n4])
 
     # n5 = Node(node_number=5, payoffs=(0,0))
-    # n2.children.append(n5)
+    # n2.children.append([n5])
 
     # n6 = Node(node_number=6, payoffs=(1,2))
-    # n2.children.append(n6)
+    # n2.children.append([n6])
 
 
     n0 = Node(node_number=0, player=0, children=[], actions=(0, 1, 2))
 
     n1 = Node(node_number=1, player=1, children=[], actions=("A", "R"))
-    n0.children.append(n1)
+    n0.children.append([n1])
 
     n2 = Node(node_number=2, player=1, children=[], actions=("A", "R"))
-    n0.children.append(n2)
+    n0.children.append([n2])
 
     n3 = Node(node_number=3, player=1, children=[], actions=("A", "R"))
-    n0.children.append(n3)
+    n0.children.append([n3])
 
     n4 = Node(node_number=4, payoffs=(2,0))
-    n1.children.append(n4)
+    n1.children.append([n4])
 
     n5 = Node(node_number=5, payoffs=(0,0))
-    n1.children.append(n5)
+    n1.children.append([n5])
 
     n6 = Node(node_number=6, payoffs=(1,1))
-    n2.children.append(n6)
+    n2.children.append([n6])
 
     n7 = Node(node_number=7, payoffs=(0,0))
-    n2.children.append(n7)
+    n2.children.append([n7])
 
     n8 = Node(node_number=8, payoffs=(0,2))
-    n3.children.append(n8)
+    n3.children.append([n8])
 
     n9 = Node(node_number=9, payoffs=(0,0))
-    n3.children.append(n9)
+    n3.children.append([n9])
 
 
     # n0 = Node(node_number=0, player=0, children=[], actions=("U", "D"))
         
     # n1 = Node(node_number=1, payoffs=(2,2))
-    # n0.children.append(n1)
+    # n0.children.append([n1])
     
     # n2 = Node(node_number=2, player=1, children=[], actions=("L", "R"))
-    # n0.children.append(n2)
+    # n0.children.append([n2])
     
     # n3 = Node(node_number=3, payoffs=(3,1))
-    # n2.children.append(n3)
+    # n2.children.append([n3])
     
     # n4 = Node(node_number=4, payoffs=(0,0))
-    # n2.children.append(n4)
+    # n2.children.append([n4])
 
     n, arr = spne([], n0)
+
+    print(n)
+    for i in range(len(n)):
+        print(n[i].payoffs)
 
     s0, s1 = [], []
     print(f"SPNE payoff: {arr[-1]["payoff"]}")
