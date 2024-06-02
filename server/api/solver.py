4780fc169405ddbe75578e18f3efad8d43c29779
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-03 02:55:10 +0530

    multiple spne: full path
    heavy coding

--- server/api/solver.py
+++ server/api/solver.py
@@ -51,30 +51,40 @@ def subnash(node: "Node"):
     maxp = [len(i)-1 for i in ch]
     perms = all_perms(maxp)
 
-    # sub_nodes: set["Node"] = set()
-    sub_nodes: list["Node"] = []
+    # outer iteration - each perm
     for p in range(len(perms)):
-        # internal_sub = set()
-        internal_sub = []
+        internal_sub: list["Node"] = []
+        actions = []
+        players = []
+        node_numbers = []
+        max_payoffs = []
+
         sub_node: "Node" = ()
         max_payoff = [float("-inf") for i in node.children[0][0].payoffs]
-        action = ""
+        
+        # inner iteration gets all possible spne in that perm
         for i in range(len(perms[p])):
             np = node.children[i][perms[p][i]].payoffs[node.player]
             if np < max_payoff[node.player]:
                 continue
             elif np > max_payoff[node.player]:
                 internal_sub = []
+                actions = []
+                max_payoffs = []
+                players = []
+                node_numbers = []
+                
             sub_node = node.children[i][perms[p][i]]
-            action = node.actions[i]
+            actions.append(node.actions[i])
             max_payoff = sub_node.payoffs
+            max_payoffs.append(max_payoff)
+            players.append(node.player)
+            node_numbers.append(node.node_number)
     
             internal_sub.append(sub_node)
-        
-        for i in range(len(internal_sub)):
-            sub_nodes.append(internal_sub[i])
 
-    return action, max_payoff, node.player, node.node_number, sub_nodes
+        # this yields all spne of that perm
+        yield actions, max_payoffs, players, node_numbers, internal_sub, perms[p]
 
 def spne(arr: list, node: "Node"):
     children = node.children
@@ -83,15 +93,35 @@ def spne(arr: list, node: "Node"):
         if children[i][0].payoffs == None:
             ret, ret_arr = spne(arr, children[i][0]) # recursively make all pre-terminal nodes
             children[i] = ret # automatically propagates to node.children
-            main_arr.extend(ret_arr)
-    action, payoff, player, num, n = subnash(node)
-    main_arr.append({
-        "action": action,
-        "payoff": payoff,
-        "player": player,
-        "num": num
-    })
-    return n, main_arr # find the nash of a pre-terminal node
+        else:
+            ret_arr = [[{
+                "payoff": children[i][0].payoffs,
+                "num": children[i][0].node_number
+            }]] # to get the lengths of node.children and main_arr same
+        main_arr.append(ret_arr)
+
+    sub_nodes: list["Node"] = []
+    final_arr = []
+    for actions, payoffs, players, nums, n, perm in subnash(node):
+        sub_nodes.extend(n)
+
+        sub_arr = []
+
+        for i in range(len(perm)):
+            sub_arr.extend(main_arr[i][perm[i]])
+
+        another_arr = []
+        for i in range(len(actions)):
+            another_arr.append([*sub_arr, {
+                "action": actions[i],
+                "payoff": payoffs[i],
+                "player": players[i],
+                "num": nums[i]
+            }])
+
+        final_arr.extend(another_arr)
+
+    return sub_nodes, final_arr
 
 def main():
     # arr = [] # dynamic programming 👍
@@ -147,6 +177,33 @@ def main():
     n3.children.append([n9])
 
 
+    # n0 = Node(node_number=0, player=0, children=[], actions=('G', 'H'))
+
+    # n1 = Node(node_number=1, player=1, children=[], actions=("C", "D"))
+    # n0.children.append([n1])
+
+    # n2 = Node(node_number=2, player=1, children=[], actions=("E", "F"))
+    # n0.children.append([n2])
+
+    # n3 = Node(node_number=3, payoffs=(2,2))
+    # n1.children.append([n3])
+
+    # n4 = Node(node_number=4, player=0, children=[], actions=("A", "B"))
+    # n1.children.append([n4])
+
+    # n5 = Node(node_number=5, payoffs=(1,1))
+    # n2.children.append([n5])
+
+    # n6 = Node(node_number=6, payoffs=(2,1))
+    # n2.children.append([n6])
+
+    # n7 = Node(node_number=7, payoffs=(3,2))
+    # n4.children.append([n7])
+
+    # n8 = Node(node_number=8, payoffs=(3,3))
+    # n4.children.append([n8])
+
+
     # n0 = Node(node_number=0, player=0, children=[], actions=("U", "D"))
         
     # n1 = Node(node_number=1, payoffs=(2,2))
@@ -163,18 +220,16 @@ def main():
 
     n, arr = spne([], n0)
 
-    print(n)
-    for i in range(len(n)):
-        print(n[i].payoffs)
-
-    s0, s1 = [], []
-    print(f"SPNE payoff: {arr[-1]["payoff"]}")
-    for i in range(len(arr)-1, -1, -1):
-        if arr[i]["player"] == 0:
-            s0.append({f"n{arr[i]["num"]}": arr[i]["action"]})
-        else:
-            s1.append({f"n{arr[i]["num"]}": arr[i]["action"]})
-    print(f"SPNE strategy profile: ({s0}, {s1})")
+    for k in range(len(arr)):
+        s0, s1 = [], []
+        print(f"SPNE payoff: {arr[k][-1]["payoff"]}")
+        for i in range(len(arr[k])-1, -1, -1):
+            if "action" in arr[k][i].keys():
+                if arr[k][i]["player"] == 0:
+                    s0.append({f"n{arr[k][i]["num"]}": arr[k][i]["action"]})
+                else:
+                    s1.append({f"n{arr[k][i]["num"]}": arr[k][i]["action"]})
+        print(f"SPNE strategy profile: ({s0}, {s1})")
 
 if __name__ == '__main__':
     main()
